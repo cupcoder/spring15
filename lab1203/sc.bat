b00efa25a83b89669737cc6e3edb2477324ef3c0
Alex0 <me@codebreak.ru>
2015-03-12 14:02:58 +0300

    last_fix
    added comment

--- lab1203/sc.bat
+++ lab1203/sc.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enableextensions
 set count=0
-:: for %%x in (*) do set /a count+=1 ::
-for %%x in ("test0\*.*") do set /a count+=1
+:: for %%x in (*) do set /a count+=1 :: Uncomment this line and comment the line below for the first task. And don't forget remove parameter from command line :D
+for %%x in ("%1\*.*") do set /a count+=1
 echo %count%
 endlocal
